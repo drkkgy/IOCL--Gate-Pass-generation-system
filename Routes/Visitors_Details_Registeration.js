26ffcdeb57aaa5923c6ef9142facf1f658824851
ankit <drkkgy@gmail.com>
2018-06-05 12:59:43 +0530

    request registration and display modules completed

--- Routes/Visitors_Details_Registeration.js
+++ Routes/Visitors_Details_Registeration.js
@@ -1,6 +1,7 @@
 //Dependencies
 var express = require('express');
 var router = express.Router();
+var timestamp = require('time-stamp');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient,assert = require('assert');
 
@@ -8,6 +9,32 @@ var MongoClient = require('mongodb').MongoClient,assert = require('assert');
 router.get('/test', function(req,res,next){
     
     res.send('API working')
+});
+// Submitting the request for an appointment to the database
+router.post('/uploading_request',(req,res,next)=> {
+
+	MongoClient.connect('mongodb://anki:1234567890@ds012198.mlab.com:12198/user_personal_details',(err,db)=>{
+     
+    assert.equal(null,err);
+    console.log("Sucessfuly connected to the mongodb client");
+    // Registring the information in the server for the visitor
+    db.collection('-->collection name<--').insertOne({
+    	"Name_of_visitor": req.body.Name_of_visitor,
+    	"Name_of_the_Host": req.body.Name_of_the_Host,
+    	"Visitors_company": req.body.Visitors_company,
+    	"Age": req.body.Age,
+    	"Visitors_Designation": req.body.Visitors_Designation,
+    	"Visitors_Address": req.body.Visitors_Address,
+    	"Purpose_Of_Visit": req.body.Purpose_Of_Visit,
+    	"Time-Stamp": timestamp('YYYY/MM/DD:mm:ss'),
+    	"Attended_Status?": true
+
+    })
+    .then((result)=>{
+    	res.json({"status":"200","message": "Visitors details registered sucessfully"});
+    })
+	});
+
 });
 
 module.exports = router
--- Routes/Visitors_List_Display.js
+++ Routes/Visitors_List_Display.js
@@ -9,5 +9,26 @@ router.get('/test', function(req,res,next){
     
     res.send('API working')
 });
+// Fetching the result to display it at the reception system
+app.get('/speed_violation_details/:Car_Number', (req,res,next)=>{
+
+MongoClient.connect('mongodb://ankit:1234567890@ds219879.mlab.com:19879/speed_analysis', (err,db)=> {
+
+    assert.equal(null,err);
+    console.log("Sucessfully connected to the mongodb client");
+    // sending the information
+ db.collection('-->collection name<--').find({Attended_Status: true}).toArray((err,result)=>{
+
+if(result == null)
+  {
+    res.json({"code": 404,"message":"No appointment pending !!"})
+  }
+  res.json({"code": 200,result});
+});
+
+    
+});
+
+});
 
 module.exports = router
